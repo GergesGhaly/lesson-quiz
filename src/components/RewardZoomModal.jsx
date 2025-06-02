efe5be55a9f9b2dc72d477aafe956cd7180e464f
GergesGhaly <gerges.ghaly1@gmail.com>
2025-06-02 13:57:22 +0300

    remove shineWrapperStyle from  RewardZoomModal component :)

--- src/components/RewardZoomModal.jsx
+++ src/components/RewardZoomModal.jsx
@@ -1,33 +1,9 @@
+// components/ImageModal.js
 import React from "react";
 import { motion } from "framer-motion";
 
 const RewardZoomModal = ({ imageSrc, altText, onClose, type = "image" }) => {
   if (!imageSrc) return null;
-
-  const shineWrapperStyle = {
-    position: "relative",
-    maxWidth: "90vw",
-    maxHeight: "90vh",
-    borderRadius: "15px",
-    overflow: "hidden",
-    // boxShadow: "0 0 20px gold",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  };
-
-  const shineOverlayStyle = {
-    position: "absolute",
-    top: 0,
-    left: "-75%",
-    width: "50%",
-    height: "100%",
-    background:
-      "linear-gradient(120deg, transparent, rgba(255, 209, 3, 0.178), transparent)", // ذهبي
-
-    transform: "skewX(-20deg)",
-    zIndex: 2,
-  };
 
   return (
     <div
@@ -47,34 +23,17 @@ const RewardZoomModal = ({ imageSrc, altText, onClose, type = "image" }) => {
       }}
     >
       {type === "image" ? (
-        <motion.div
+        <motion.img
+          src={imageSrc}
+          alt={altText || "reward enlarged"}
           initial={{ scale: 0 }}
           animate={{ scale: 1 }}
           transition={{ duration: 0.5 }}
-          style={shineWrapperStyle}
-        >
-          <img
-            src={imageSrc}
-            alt={altText || "reward enlarged"}
-            style={{
-              width: "100%",
-              height: "100%",
-              display: "block",
-              objectFit: "contain",
-              zIndex: 1,
-            }}
-          />
-          <motion.div
-            style={shineOverlayStyle}
-            initial={{ left: "-75%" }}
-            animate={{ left: "125%" }}
-            transition={{
-              duration: 2,
-              ease: "easeInOut",
-              // repeat: Infinity,
-            }}
-          />
-        </motion.div>
+          style={{
+            maxWidth: "80vw",
+            maxHeight: "80vh",
+          }}
+        />
       ) : (
         <motion.span
           initial={{ scale: 0 }}
@@ -83,9 +42,7 @@ const RewardZoomModal = ({ imageSrc, altText, onClose, type = "image" }) => {
           style={{
             fontSize: "150px",
             color: "#fff",
-            textShadow: "0 0 15px gold",
-            position: "relative",
-            zIndex: 1,
+            textShadow: "0 0 10px gold",
           }}
         >
           {imageSrc}
